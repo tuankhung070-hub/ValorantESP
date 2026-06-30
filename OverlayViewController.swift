97c35acc652aec6b7179bcbd0780598bf51f5558
tuankhung070-hub <tuankhung070@gmail.com>
2026-06-30 17:46:47 +0700

    Create OverlayViewController.swift

---
+++ OverlayViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class OverlayViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+        view.isUserInteractionEnabled = false
+    }
+}
